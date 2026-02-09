9e6ff5578fea0c72d26f040bee706347d5603b1b
Sombereric <Emoutoux6938@conestogac.on.ca>
2026-02-09 17:31:12 -0500

    file structure

---
+++ GuessingGameServer/TCP_Connection/ServerListener/ConnectionProtocol.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGameServer.TCP_Connection.ServerListener
+{
+    internal class ConnectionProtocol
+    {
+        //code from protocol will be something from the client like 200, 300, something like that
+        //this is what will be turned into a task to allow for multiple connections at once 
+        private void tempFunction(int codeFromProtocol)
+        {
+            switch (codeFromProtocol) 
+            {
+                //as an example. could be start game 
+                case 200:
+                    //creates state bag and picks a random file
+                    //each one of these will have the proper function call for the data that they should get
+                    //for example this will handle the guid and other such items
+                    //check the read.me for the details of each protocol
+                    break;
+                case 201:
+                    //could be guess made and calls the correct files
+                    guessMade("Help");
+                    break;
+            }
+            return;
+        }
+        // a example method that can handle 201 protocol code. this can change but gives the idea
+        private void guessMade(string guess)
+        {
+            //this would be where the guess is handled against the state bag. 
+            //this is why state bags needs to be a class as to allow for us to create a list
+        }
+    }
+}
