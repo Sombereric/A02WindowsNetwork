b601b371c54c796d7b2b8b06a941ba4e71734f99
Sombereric <Emoutoux6938@conestogac.on.ca>
2026-02-12 12:33:30 -0500

    Update ServerMainRunner.cs

--- GuessingGameServer/ServerMainRunner.cs
+++ GuessingGameServer/ServerMainRunner.cs
@@ -7,10 +7,11 @@ namespace GuessingGameServer
     internal class ServerMainRunner
     {
         private static readonly List<GameStateInfo> gameStatesInfo = new List<GameStateInfo>();
+        public readonly object GameStateLocker = new object();
         public void mainRunner() 
         {
             ConnectionHandler connectionHandler = new ConnectionHandler();
-            Task clientHandlerTask = Task.Run(async () => await connectionHandler.MainServerListener(gameStatesInfo));
+            Task clientHandlerTask = Task.Run(async () => await connectionHandler.MainServerListener(gameStatesInfo, GameStateLocker));
 
             ServerClientSender sender = new ServerClientSender();
             //Task clientHandlerTask = Task.Run(async () => await connectionHandler.MainServerListener(gameStatesInfo));
