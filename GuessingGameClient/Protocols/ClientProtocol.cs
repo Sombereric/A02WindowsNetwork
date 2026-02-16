0022c99767046e7e1df93d8ba3a34773d6d11a1c
Sombereric <Emoutoux6938@conestogac.on.ca>
2026-02-15 19:11:38 -0500

    moved the files from console app to the wpf

---
+++ GuessingGameClient/Protocols/ClientProtocol.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientUI.Protocols
+{
+    class ClientProtocol
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="checkDictionary"></param>
+        /// <returns></returns>
+       public static string BuildMessage(Dictionary<string, string> checkDictionary)
+       {
+            StringBuilder sb = new StringBuilder();
+            
+            foreach (KeyValuePair<string, string> checkKey in checkDictionary)
+            {
+                sb.AppendLine(checkKey.Key + ":"+ checkKey.Value);
+            }
+            sb.AppendLine("END");
+
+            return sb.ToString();
+       }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rawText"></param>
+        /// <returns></returns>
+        public static Dictionary<string, string> RawMessage(string rawText)
+        {
+            Dictionary<string, string> checkFields = new Dictionary<string, string>();
+
+            string[] lookLines = rawText.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string lookLine in lookLines)
+            {
+                if (lookLine.Trim() == "END")
+                {
+                    break;
+                }
+                string[] checkParts = lookLine.Split(':', 2);
+
+                if (checkParts.Length == 2)
+                {
+                    string lookKey = checkParts[0].Trim();
+                    string lookValue = checkParts[1].Trim();
+
+                    checkFields[checkParts[0].Trim()] = checkParts[1].Trim();
+                }
+            }
+            return checkFields;
+        }
+    }
+}
---
+++ GuessingGameClient/TCP Connection/ClientConnection.cs
@@ -0,0 +1,143 @@
+﻿/*
+* FILE : ClientConnection.cs
+* PROJECT : PROG2126 - Assignment #2
+* PROGRAMMER : Eric Moutoux, Will Jessel, Zemmatt Hagos
+* FIRST VERSION : 2026-2-9
+* DESCRIPTION :
+* handles each request made by the client 
+*
+*/
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using ClientUI.Protocols;
+
+namespace ClientUI.TCP_Connection
+{
+    class ClientConnection
+    {
+        private TcpClient tcpClient;
+        private StreamReader reader;
+        private StreamWriter writer;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private async Task ConnectTheClient(string needIP, string needPort)
+        {
+            tcpClient = new TcpClient();
+
+            IPAddress address = IPAddress.Parse(needIP);
+
+            if(!int.TryParse(needPort, out int port))
+            {
+                throw new Exception("Invalid port number");
+            }
+
+            await tcpClient.ConnectAsync(address, port);
+
+     
+            NetworkStream stream = tcpClient.GetStream();
+            
+            reader = new StreamReader(stream);
+            
+            writer = new StreamWriter(stream);
+            
+            writer.AutoFlush = true;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="lookMessage"></param>
+        /// <returns></returns>
+        private async Task SendMessage(string lookMessage)
+        {
+            string[] lookLines = lookMessage.Split(Environment.NewLine);
+       
+
+            foreach(string checkLine in lookLines)
+            {
+                await writer.WriteLineAsync(checkLine);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private async Task <string> ReadMessageAtEnd()
+        {
+            StringBuilder sb = new StringBuilder();
+            string checkLine;
+
+            while (true)
+            {
+                checkLine = await reader.ReadLineAsync();
+                
+                if(checkLine == null)
+                {
+                    throw new Exception("Connection closed");
+        
+
+                }
+                else if(checkLine == "END")
+                {
+                    break;
+                }
+
+                sb.AppendLine(checkLine);
+            }
+
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="port"></param>
+        /// <param name="fieldsDictionary"></param>
+        /// <returns></returns>
+        public async Task <string> CheckRequestResponse(string ip, string port, Dictionary<string, string> fieldsDictionary)
+        {
+            await ConnectTheClient(ip, port);
+
+            string checkMsg = ClientProtocol.BuildMessage(fieldsDictionary);
+
+            await SendMessage(checkMsg);
+
+            string checkResponse = await ReadMessageAtEnd();
+
+            tcpClient.Close();
+
+            return checkResponse;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void CloseClient()
+        {
+            reader?.Dispose();
+            writer?.Dispose();
+            tcpClient.Close();
+        }
+        
+
+    }
+
+    
+    
+
+
+}
